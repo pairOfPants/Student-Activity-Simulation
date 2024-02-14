c40b4b0d79852f283ea217efab1999666e00e553
PairOfPants <adenham112@gmail.com>
2024-02-14 18:12:03 -0500

    Added Map and Test Objects
    Began to work on mouse-movement

--- Student Activity Simulation/Assets/Scripts/Activity.cs
+++ Student Activity Simulation/Assets/Scripts/Activity.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Activity : MonoBehaviour
 { 
-    protected string Name; //Name of activity
-    protected System.DateTime StartTime; //Start time in hours,seconds format
-    protected System.DateTime EndTime; //End time in hours,seconds format
-    protected int NumOfDays; //Number of days during the week this event occurs (clamped between 0 and 7)
-    protected bool[] Weekdays = {false,false,false,false,false,false,false}; //does this event happen yes or no for each day of the week
-    protected Place PlaceOfOccurrence; //place on the map this event occurs
-    protected int Priority; //if two events overlap, schedule will choose event with higher priority
-    protected int NumOfTimesCompleted; //if two events overlap with same priority, schedule will do the event done less times
+    [SerializeField]
+    private string Name; //Name of activity
+    [SerializeField]
+    private Time StartTime; //Start time in hours,minutes format
+    [SerializeField]
+    private Time EndTime; //End time in hours,minutes format
+    [SerializeField]
+    private int NumOfDays; //Number of days during the week this event occurs (clamped between 0 and 7)
+    [SerializeField]
+    private bool[] Weekdays = {false,false,false,false,false,false,false}; //does this event happen yes or no for each day of the week
+    [SerializeField]
+    private Place PlaceOfOccurrence; //place on the map this event occurs
+    [SerializeField]
+    private int Priority; //if two events overlap, schedule will choose event with higher priority
+    [SerializeField]
+    private int NumOfTimesCompleted; //if two events overlap with same priority, schedule will do the event done less times
 
 
     // Start is called before the first frame update
---
+++ Student Activity Simulation/Assets/Scripts/Time.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Time : MonoBehaviour
+{
+    [SerializeField]
+    protected int hour;
+    [SerializeField]
+    protected int minute;
+    // Start is called before the first frame update
+    public Time(int h, int m)
+    {
+        hour = Mathf.Clamp(h, 1, 12);;
+        minute = Mathf.Clamp(m, 1, 12);;
+    }
+    public string toString()
+    {
+        string ret = (hour + ":" + minute);
+        return ret;
+    }
+}
